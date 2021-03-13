67f5839da838ea722b38a4a67ed2dcd5561bc101
realntha <77433459+realntha@users.noreply.github.com>
2021-03-13 07:59:46 +0700

    make it better

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 var publicDir = require('path').join(__dirname,'/public');
 app.use(express.static(publicDir));
 
-//npm i handlebars consolidate --save
+
 app.engine('hbs',engines.handlebars);
 app.set('views','./views');
 app.set('view engine','hbs');
 
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb+srv://hoaan:123456abc@cluster0.yvnfp.gcp.mongodb.net/test';
+var url = 'mongodb+srv://hoaan:123456abc@cluster0.jfaxd.mongodb.net/MyStore';
 
 app.get('/all',async function(req,res){
     let client= await MongoClient.connect(url);
@@ -33,6 +33,7 @@ app.get('/delete',async function(req,res){
 
     let client= await MongoClient.connect(url);
     let dbo = client.db("MyStore");
+
     await dbo.collection("MyStore").deleteOne(condition);
     res.redirect('/all');
 })
@@ -44,17 +45,16 @@ app.get('/insert',async function(req,res){
 app.post('/doInsert',async function (req,res){
     let client= await MongoClient.connect(url);
     let dbo = client.db("MyStore");
-    let name = req.body.txtName;
-    let price = req.body.txtPrice;
-    let description = req.body.txtDescription;
-    let newProduct = {name : name, price : price, description:description};
+    let Name = req.body.txtName;
+    let Price = req.body.txtPrice;
+    let Description = req.body.txtDescription;
+    let newProduct = {name : Name, price : Price, description: Description};
     await dbo.collection("MyStore").insertOne(newProduct);
     console.log(newProduct);
     
     res.redirect('/all');
 });
 
-
-var server = app.listen(process.env.PORT||5000,function(){
-    console.log("Server is running on PORT 5000");
-})
+var PORT = process.env.PORT || 5000
+app.listen(PORT);
+console.log("Server is running at " + PORT)
